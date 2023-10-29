6f53e0f12dc7a533b35e36d283728aa9cf2d3c9e
Jeremie Graulle <jeremie.graulle@epita.fr>
2023-10-29 02:21:24 +0200

    Fill IrProximitySensor update step 2

--- src/irProximitySensor.cpp
+++ src/irProximitySensor.cpp
@@ -36,8 +36,11 @@ void IrProximitySensor::update(float, const sf::Transform & parentWorldTransform
             break;
     }
 
-    // TODO Step2 if _distanceDetected < _distanceMin update _distanceDetected
-    // (and _line[1].position) to have the same behavior as reel IR sensor
+    if (_distanceDetected<_distanceMin)
+    {
+        _distanceDetected = _distanceMax-(_distanceMax-_distanceMin)*_distanceDetected/_distanceMin;
+        _line[1].position = worldTranform.transformPoint(sf::Vector2f(_distanceDetected, 0.0));
+    }
 }
 
 void IrProximitySensor::draw(sf::RenderTarget & target, sf::RenderStates states) const
