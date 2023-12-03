d6555760b413fd65bd7039cc53399e7c1035ccf4
Jeremie Graulle <jeremie.graulle@epita.fr>
2023-12-03 21:51:44 +0100

    Add friction force to move robot

--- src/robot.cpp
+++ src/robot.cpp
@@ -5,12 +5,16 @@
 #include <SFML/Graphics/RenderTarget.hpp>
 #include <numeric>
 #include <numbers>
+#include <iostream>
+#include <iomanip>
+#include <cmath>
 
 
 Robot::Robot(const sf::Vector2f position, const sf::Vector2f & size, float wheelsDistance, const Map & map)
     : _size(size)
     , _wheelsDistance(wheelsDistance)
     , _motorsPower({0.0, 0.0})
+    , _motorsSpeed({0.0, 0.0})
     , _shape(std::make_unique<sf::RectangleShape>(size))
     , _sensors()
     , _map(map)
@@ -33,14 +37,62 @@ Robot::MotorIndex Robot::motorIndexFromString(const std::string & str)
     throw std::invalid_argument(std::string("Cannot convert ") + str + " into Robot::MotorIndex");
 }
 
+void Robot::setMotorsPower(float rightValue, float leftValue)
+{
+    if (!checkMotorPowerValue(rightValue))
+        return;
+    if (!checkMotorPowerValue(leftValue))
+        return;
+    _motorsPower = {rightValue, leftValue};
+}
+
+void Robot::setMotorPower(MotorIndex motorIndex, float value)
+{
+    if (!checkMotorPowerValue(value))
+        return;
+    _motorsPower[static_cast<std::size_t>(motorIndex)] = value;
+}
+
 void Robot::update(float elapsedTime)
 {
+    // Compute new speed of each motor
+    for (auto motorIndex = 0u; motorIndex<MOTORS_COUNT; motorIndex++)
+    {
+        float motorForce = _motorsPower.at(motorIndex)*MOTOR_FORCE_MAX;
+        // Cap speed max from current PWM by motor force
+        if (std::abs(_motorsSpeed.at(motorIndex))>std::abs(MOTOR_SPEED_MAX*_motorsPower.at(motorIndex)))
+            motorForce = 0.0;
+        // Compute frictional force
+        double frictionalForce = 0.0;
+        if (_motorsSpeed.at(motorIndex) == 0.0 && std::abs(_motorsPower.at(motorIndex)) < MOTOR_POWER_MIN_TO_START)
+            frictionalForce = motorForce;
+        else if (motorForce == 0.0)
+            frictionalForce = std::copysign(MASS * GRAVITY * FRICTION_DYNAMIC_COEF, _motorsSpeed.at(motorIndex));
+        // Compute net force
+        double netForce = motorForce - frictionalForce;
+        // Compute acceleration
+        double acceleration = netForce / MASS;
+        // Compute speed
+        double newSpeed = _motorsSpeed.at(motorIndex) + acceleration*elapsedTime;
+        // Frictional cannot change speed sign
+        if (motorForce==0 && std::signbit(newSpeed)!=std::signbit(_motorsSpeed.at(motorIndex)))
+            newSpeed = 0.0;
+#ifdef SPEED_DEBUG
+        if (motorIndex == 0u)
+            std::cout << std::fixed << std::setprecision(3)
+                    << "speed=" << std::setw(8) << _motorsSpeed.at(motorIndex) << " pwm=" << _motorsPower.at(motorIndex)
+                    << " power=" << std::setw(8) << motorForce << " frictionalForce=" << std::setw(8) << frictionalForce
+                    << " netForce=" << std::setw(8) << netForce << " newSpeed=" << std::setw(8) << newSpeed << std::endl;
+#endif
+        _motorsSpeed.at(motorIndex) = newSpeed;
+    }
+
     // Compute new position and orientation
     sf::Transform orientationTransform(sf::Transform().rotate(getRotation()));
-    float linearStep = (getMotorPower(MotorIndex::LEFT) + getMotorPower(MotorIndex::RIGHT))
-            / 2.0 * elapsedTime * MOTOR_POWER_TO_SPEED;
-    float angularStep = (getMotorPower(MotorIndex::LEFT) - getMotorPower(MotorIndex::RIGHT))
-            / _wheelsDistance * elapsedTime * MOTOR_POWER_TO_SPEED * 180.0 / std::numbers::pi;
+    float linearStep = (getMotorSpeed(MotorIndex::LEFT) + getMotorSpeed(MotorIndex::RIGHT))
+            / 2.0 * elapsedTime;
+    float angularStep = (getMotorSpeed(MotorIndex::LEFT) - getMotorSpeed(MotorIndex::RIGHT))
+            / _wheelsDistance * elapsedTime * 180.0 / std::numbers::pi;
     Transformable newTransform;
     newTransform.setPosition(getPosition() + orientationTransform.transformPoint(sf::Vector2f(linearStep, 0.0)));
     newTransform.setRotation(getRotation() + angularStep);
@@ -89,5 +141,21 @@ void Robot::reset()
 {
     setPosition(_positionInit);
     setRotation(0.0);
-    setMotorsPower(0.0, 0.0);
+    for (auto motorIndex = 0u; motorIndex<MOTORS_COUNT; motorIndex++)
+    {
+        _motorsPower[motorIndex] = 0.0;
+        _motorsSpeed[motorIndex] = 0.0;
+    }
+}
+
+bool Robot::checkMotorPowerValue(float value)
+{
+    if (value < -1.0 || 1.0 < value)
+    {
+        std::cerr << "Try to set motor power with invalid value " << value
+                << " value must be between " << -1.0 << " and " << 1.0
+                << " command ignored";
+        return false;
+    }
+    return true;
 }
--- src/robot.hpp
+++ src/robot.hpp
@@ -20,7 +20,14 @@ namespace sf
 class Robot : public sf::Drawable, public sf::Transformable
 {
 public:
-    static constexpr float MOTOR_POWER_TO_SPEED = 100.0;
+    static constexpr float MASS = 0.5; // In kg
+    static constexpr float GRAVITY = 9.81; // In m/s^2
+    static constexpr float FRICTION_DYNAMIC_COEF = 40.0;
+    static constexpr float MOTOR_POWER_MIN_TO_START = 0.1; // PWM between -1.0 and 1.0
+    static constexpr float MOTOR_FORCE_MAX = 500.0; // In kg.m/s^2
+    static constexpr float MOTOR_SPEED_MAX = 200.0; // In pixel / second
+    enum class MotorIndex {RIGHT = 0, LEFT = 1};
+    static constexpr std::size_t MOTORS_COUNT = 2;
 
     //! @param size: in pixel
     Robot(const sf::Vector2f position, const sf::Vector2f & size, float wheelsDistance, const Map & wall);
@@ -31,7 +38,6 @@ public:
     inline const auto & getSensors() const
             {return _sensors;}
 
-    enum class MotorIndex {RIGHT = 0, LEFT = 1};
     static MotorIndex motorIndexFromString(const std::string & str);
 
     //! @return PWM between -1.0 and 1.0
@@ -43,9 +49,8 @@ public:
 
     //! @param value: PWM between -1.0 and 1.0
     //! \{
-    inline void setMotorsPower(float rightValue, float leftValue) {_motorsPower = {rightValue, leftValue};}
-    inline void setMotorPower(MotorIndex motorIndex, float value)
-            {_motorsPower[static_cast<std::size_t>(motorIndex)] = value;}
+    void setMotorsPower(float rightValue, float leftValue);
+    void setMotorPower(MotorIndex motorIndex, float value);
     //! \}
 
     //! @param elapsedTime: in seconds
@@ -56,9 +61,14 @@ public:
     void reset();
 
 private:
+    static bool checkMotorPowerValue(float value);
+    inline float getMotorSpeed(MotorIndex motorIndex) const
+            {return _motorsSpeed.at(static_cast<std::size_t>(motorIndex));}
+
     const sf::Vector2f _size;
     const float _wheelsDistance;
-    std::array<float, 2> _motorsPower;
+    std::array<float, MOTORS_COUNT> _motorsPower;
+    std::array<float, MOTORS_COUNT> _motorsSpeed;
     std::unique_ptr<sf::Shape> _shape;
     std::vector<std::unique_ptr<Sensor>> _sensors;
     const Map & _map;
