ff9bada704baf74e65007f5f8c7b7e85265e8cc8
Jeremie Graulle <jeremie.graulle@epita.fr>
2024-02-12 00:42:12 +0100

    Add labyrinth map and set mininal distance for distance sensor so 0

--- src/main.cpp
+++ src/main.cpp
@@ -70,15 +70,15 @@ int main(int argc, char** argv)
     //window.setFramerateLimit(60);
 
     // Build a robot
-    Robot robot(sf::Vector2f(window.getView().getSize().x/2.0, 225.0), sf::Vector2f(20.0, 20.0), 20.0, map);
-    robot.addSensor(std::make_unique<IrProximitySensor>(10.0, 80.0, sf::Vector2f(10.0, 10.0), 90.0, map));
-    robot.addSensor(std::make_unique<IrProximitySensor>(10.0, 80.0, sf::Vector2f(10.0, -10.0), -90.0, map));
-    robot.addSensor(std::make_unique<IrProximitySensor>(10.0, 80.0, sf::Vector2f(-10.0, 10.0), 90.0, map));
-    robot.addSensor(std::make_unique<IrProximitySensor>(10.0, 80.0, sf::Vector2f(-10.0, -10.0), -90.0, map));
+    Robot robot(sf::Vector2f(90.0, 90.0), sf::Vector2f(20.0, 20.0), 20.0, map);
+    robot.addSensor(std::make_unique<IrProximitySensor>(0.0, 80.0, sf::Vector2f(10.0, 10.0), 90.0, map));
+    robot.addSensor(std::make_unique<IrProximitySensor>(0.0, 80.0, sf::Vector2f(10.0, -10.0), -90.0, map));
+    robot.addSensor(std::make_unique<IrProximitySensor>(0.0, 80.0, sf::Vector2f(-10.0, 10.0), 90.0, map));
+    robot.addSensor(std::make_unique<IrProximitySensor>(0.0, 80.0, sf::Vector2f(-10.0, -10.0), -90.0, map));
     robot.addSensor(std::make_unique<LineTrackSensor>(sf::Vector2f(5.0, 0.0), map, 128u));
     robot.addSensor(std::make_unique<SwitchSensor>(sf::Vector2f(11.0, 10.0), 3, -90.0, map));
     robot.addSensor(std::make_unique<SwitchSensor>(sf::Vector2f(11.0, -10.0), 3, 90.0, map));
-    robot.addSensor(std::make_unique<UltrasoundSensor>(10.0, 400.0, 30.0, sf::Vector2f(10.0, 0.0), 0.0, map));
+    robot.addSensor(std::make_unique<UltrasoundSensor>(0.0, 400.0, 30.0, sf::Vector2f(10.0, 0.0), 0.0, map));
     robot.addSensor(std::make_unique<EncoderWheelSensor>(sf::Vector2f(0.0, 5.0), 6.5, 20u));
     robot.addSensor(std::make_unique<EncoderWheelSensor>(sf::Vector2f(0.0, -5.0), 6.5, 20u));
     RobotCommand robotCommand(robot, tcpPort);
