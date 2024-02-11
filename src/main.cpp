58705c5ed509618dcc949a9a9c24b96a781d4be1
Jeremie Graulle <jeremie.graulle@epita.fr>
2024-02-11 14:53:06 +0100

    Update IR distance sensor to be able to follow wall on each side

--- src/main.cpp
+++ src/main.cpp
@@ -67,8 +67,10 @@ int main(int argc, char** argv)
 
     // Build a robot
     Robot robot(sf::Vector2f(window.getView().getSize().x/2.0, 225.0), sf::Vector2f(20.0, 20.0), 20.0, map);
-    robot.addSensor(std::make_unique<IrProximitySensor>(10.0, 80.0, sf::Vector2f(10.0, 10.0), 0.0, map));
-    robot.addSensor(std::make_unique<IrProximitySensor>(10.0, 80.0, sf::Vector2f(10.0, -10.0), 0.0, map));
+    robot.addSensor(std::make_unique<IrProximitySensor>(10.0, 80.0, sf::Vector2f(10.0, 10.0), 90.0, map));
+    robot.addSensor(std::make_unique<IrProximitySensor>(10.0, 80.0, sf::Vector2f(10.0, -10.0), -90.0, map));
+    robot.addSensor(std::make_unique<IrProximitySensor>(10.0, 80.0, sf::Vector2f(-10.0, 10.0), 90.0, map));
+    robot.addSensor(std::make_unique<IrProximitySensor>(10.0, 80.0, sf::Vector2f(-10.0, -10.0), -90.0, map));
     robot.addSensor(std::make_unique<LineTrackSensor>(sf::Vector2f(5.0, 0.0), map, 128u));
     robot.addSensor(std::make_unique<SwitchSensor>(sf::Vector2f(11.0, 10.0), 3, -90.0, map));
     robot.addSensor(std::make_unique<SwitchSensor>(sf::Vector2f(11.0, -10.0), 3, 90.0, map));
